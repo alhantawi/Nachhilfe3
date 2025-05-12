2e481668c4c89c7931e939ba64f4b410b473a503
alhantawi <82392782+alhantawi@users.noreply.github.com>
2025-05-12 15:42:56 +0200

    add email

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -82,7 +82,7 @@ export function HeroSection() {
                 variant="outline" 
                 size="lg" 
                 className="border-white/30 text-black hover:text-white cursor-pointer hover:bg-white/10 rounded-full"
-              > <a href="mailto:example@example.com?subject=Becoming an Instructor">
+              > <a href="mailto:alhantawi@nachhilfelight.de?subject=Becoming an Instructor">
                 {t("cta.become")}
                 </a>
               </Button>
